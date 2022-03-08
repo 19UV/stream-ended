1728596047ee38c77b7f6f32c752ff34a49383c4
19UV <the19UV@gmail.com>
2022-03-07 21:32:56 -0500

    Added discord customization to the yaml

--- config.go
+++ config.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"strings"
+	"text/template"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -9,11 +11,11 @@ import (
 type BotConfig struct {
 	Discord struct {
 		Webhook string `yaml:"webhook"`
+		Message string `yaml:"message"`
 	}
 
 	Twitch struct {
 		Id string `yaml:"id"`
-		
 		User string `yaml:"user"`
 	}
 }
@@ -49,6 +51,22 @@ func ParseConfig() (*BotConfig, error) {
 	if config.Twitch.User == "" {
 		return nil, ConfigError{ "twitch.user required, but not included" }
 	}
+
+	if config.Discord.Message == "" {
+		config.Discord.Message = "@everyone {{.User}} just went Offline!"
+	}
+
+	template, err := template.New("message").Parse(config.Discord.Message)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := new(strings.Builder)
+	err = template.Execute(buf, map[string]string{ "User": config.Twitch.User })
+	if err != nil {
+		return nil, err
+	}
+	config.Discord.Message = buf.String()
 
 	return &config, nil
 }
